30f6e096648c1a158b34997fb22de2a8b246e06e
yasminfathimaa9 <yasminfathimaa9@gmail.com>
2021-12-23 16:32:01 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Breaking from "./Breaking"
+import axios from "./axios"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+    const getAllCharecters=()=>{
+      axios.get("http://www.breaingbadapi.com/api/charecters")
+    .then((res) => console.log(res))
+    .catch((err)=> console.log(err))
+    }
+    return (
+      <div className="App">
+      <Breaking />
+      </div>
+ 
+   
   );
 }
 
---
+++ src/Breaking.js
@@ -0,0 +1,6 @@
+function Breaking(){
+    return(
+        <h1>hello</h1>
+    )
+}
+export default Breaking;
